894c00430abb13d95432d3302ec881e258e44c93
rbulanad <117868489+rbulanad@users.noreply.github.com>
2022-11-22 16:21:50 +0100

    Add files via upload

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbulanad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 14:04:45 by rbulanad          #+#    #+#             */
+/*   Updated: 2022/11/22 14:24:25 by rbulanad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*ret;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	ret = malloc ((ft_strlen(s) + 1) * sizeof(char));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ret[i] = (char)(*f)(i, s[i]);
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
